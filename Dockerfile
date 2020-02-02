1b19070fca1f051744b88d0825453b301bb45221
David Zeng <david.zeng@epitech.eu>
2020-02-02 13:15:09 +0100

    added test serialization

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.7.6-slim-stretch
 
 WORKDIR /app
-COPY ./ ./
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-ENV FLASK_APP src/app.py
+ENV FLASK_APP app/app.py
 ENV FLASK_RUN_HOST 0.0.0.0
+
+COPY ./ ./
 
 CMD ["flask", "run"]
--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
+
+class Circuit:
+    def __init__(self, id, type, option):
+        self.id = id
+        self.type = type
+        self.option = option
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
 @app.route('/getCircuits')
 def getCircuits():
-    return 'CIRCUITS'
+    test = Circuit(1, "type", "option")
+    return test.toJSON()
 
 @app.route('/getQTP')
 def getQTP():
